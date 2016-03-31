0ad0aa9102345a17630800b3ad030d2f45a9bfef
greenteawarrior <emily.wang@students.olin.edu>
2016-03-31 17:22:36 -0400

    ADD: FireFirefighterJacket data, jupyter notebook, figures

---
+++ FirefighterJacket/data/DSC/try.py
@@ -0,0 +1,5 @@
+p = "Time (min), Temperature (C), Heat Capacity (mJ/C), Rev Cp (mJ/C), Nonrev Cp (mJ/C), Heat Flow (mW), Rev Heat Flow (mW), Nonrev Heat Flow (mW),Temperature Amplitude (C),Heat Flow Amplitude (mW),Heat Flow Phase (rad),Modulated Temperature (C),Modulated Heat Flow (mW),Reference Sine Angle (rad),Sample Purge Flow (mL/min)"
+
+print p.split(",")
+
+
